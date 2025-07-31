922c827cdfcf572f317a0fe242c9a87b91fcc31b
Zain-Assi-73 <assizain73@gmail.com>
2025-07-31 20:45:15 +0400

    For shada

---
+++ index.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById("btn")
+const div1 = document.getElementById("div1")
+
+
+function style (){
+    div1.style.display="block"
+}
+btn.addEventListener("click" , style)
